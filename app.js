c29886e07bc3d13f2f2a4b5cd11f42471865def1
Abdulrahman Khalid <47614266+anthibo@users.noreply.github.com>
2021-08-22 13:35:14 +0200

    implementing cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean')
 const hpp = require('hpp')
 const cookieParser = require('cookie-parser')
 const compression = require('compression')
+const cors = require('cors')
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -30,8 +31,17 @@ if (process.env.NODE_ENV === 'development') {
 
 
 // global middlwares
+//implement cors for all clients for simple requests
+app.use(cors())
+
+// api.natours.com, natours.com
+// app.use(cors({ origin: 'https://natours.com' }))
+
 //serving static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.options('*', cors())
 // set security http headers
 app.use(
   helmet.contentSecurityPolicy({
